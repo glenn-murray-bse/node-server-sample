c57276ae3ce20631242cab6b37402175525fbca7
Glenn Murray <glenn.murray.bse@gmail.com>
2014-06-21 18:46:14 +1000

    Formatting

--- server.js
+++ server.js
@@ -9,27 +9,27 @@ db.connect();
 
 var render = require('./lib/render');
 
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var params = url.parse(req.url, true).query;
     var end = res.end.bind(res);
     if (req.url === '/') {
         render('index.html', end);
     } else if (req.url === '/db') {
-        db.query('select * from users where ? LIMIT 1', {name: params.name}, function(err, rows, field){
+        db.query('select * from users where ? LIMIT 1', {name: params.name}, function(err, rows, field) {
             if(err) {throw err;}
             render('index.html', { rows: rows }, end);
         });
     } else if (req.url === '/remote') {
-        http.request(params, function(response){
+        http.request(params, function(response) {
             response.on('data', res.write.bind(res));
             response.on('end', end);
         });
     } else {
         render(req.url, { input: params.input }, end);
     }
-}).listen(80, function(err) {
+}).listen(8080, function(err) {
     if(err) {throw err;}
-    console.log('Server running at http://127.0.0.1:80/');
+    console.log('Server running at http://localhost:' + server.address().port + '/');
 });
 console.log('Server starting');
